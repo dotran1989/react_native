629a7d8480a821101ab23ac0d7eed40b12809090
cuongquoc2710 <cuongdnq@proteusion.com>
2018-04-15 10:34:04 +0700

    textInput multiline and listener

--- reactnative/tutorial/components/10_textInput/textInput.js
+++ reactnative/tutorial/components/10_textInput/textInput.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, Keyboard } from 'react-native';
 
 export default class textInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
             typeText: 'please type your text',
-            typePassword: ''
+            typePassword: '',
+            typeDescription: 'your description'
         };
     }
+    
+    componentWillMount() {
+        this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+            this.setState(() => {
+                return {typeText: 'keyboard is shown'}
+            })
+        });
+        this.keyboardHideShowListener = Keyboard.addListener('keyboardDidHide', () => {
+            this.setState(() => {
+                return {typeText: 'keyboard is hide'}
+            })
+        });
+    }
+
+    componentWillUnmount() {
+        this.keyboardDidShowListener.remove();
+        this.keyboardHideShowListener.remove();
+    }
+
     render() {
         return (
             <View>
@@ -60,6 +80,34 @@ export default class textInput extends Component {
                 >
                 </TextInput>
                 <Text style={{marginLeft: 20}}>{this.state.typePassword}</Text>
+                <TextInput style={{
+                    height: 100,
+                    margin: 20,
+                    padding: 10,
+                    borderColor: 'gray',
+                    borderWidth: 1
+                }}
+                multiline={true}
+                borderBottomColor='green'
+                borderBottomWidth={3}
+                borderLeftColor='green'
+                borderLeftWidth={3}
+                editable={true}
+                returnKeyLabel='Go'
+                returnKeyType='previous'
+                onSubmitEditing={Keyboard.dismiss}
+                onChangeText={
+                    (text) => {
+                        this.setState((previousState) => {
+                            return {
+                                typeDescription: text
+                            };
+                        })
+                    }
+                }
+                >
+                </TextInput>
+                <Text style={{marginLeft: 20}}>{this.state.typeDescription}</Text>
             </View>
         );
     }
