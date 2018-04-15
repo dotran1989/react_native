200fee7bbacfbb53a59901349b10746c3fb0ebaf
cuongquoc2710 <cuongdnq@proteusion.com>
2018-04-15 11:21:45 +0700

    using button from react-native-button

--- reactnative/tutorial/components/11_handle_button/button.js
+++ reactnative/tutorial/components/11_handle_button/button.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { Alert, View, Button, Keyboard } from 'react-native';
+import { Alert, View, Keyboard } from 'react-native';
+import Button from 'react-native-button';
 
 export default class button extends Component {
     _onPressButton = () => {
         Alert.alert("You pressed the button!");
     }
-    
+
     render() {
         return (
             <View style={styles.container}>
-                <Button onPress={this._onPressButton}
+                {/* <Button onPress={this._onPressButton}
                     title='This is a button'
                     color='green'
                 >
+                </Button> */}
+                <Button style={styles.buttonStyle}
+                    onPress={this._onPressButton}>
+                    This is a button
                 </Button>
             </View>
         );
@@ -24,5 +29,12 @@ const styles = {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    buttonStyle: {
+        fontSize: 20,
+        color: 'white',
+        backgroundColor: 'green',
+        padding: 15,
+        borderRadius: 20
     }
 };
